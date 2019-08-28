af9fcd34d7b05d992cafaa8326e56d1c16ae64c5
CuzICan2 <niclas.strandeus@gmail.com>
2019-08-28 18:15:21 +0200

    built the facerecognition shell

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Rank = () => {
+    return (
+       <div>
+            <div className='f3 white'>
+                {'Niclas, your current rank is...'}
+            </div>
+            <div className='f1 white'>
+                {'#5'}
+            </div>
+       </div>
+/*        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className='center f3 link dim black underline pa3 pointer'>Rank #</p>
+        </nav> */
+    );
+}
+
+export default Rank; 
